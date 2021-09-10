56e6c79602bc42eb703f7b2b1b742875c92aca18
EvgenyHvostin <evgenyzhvostin@gmail.com>
2021-09-10 15:11:28 +0300

    Big Update

--- src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/ArrayTaskList.java
@@ -1,64 +1,72 @@
 package ua.edu.sundu.j2se.studentHvostin.tasks;
 
-import java.util.ArrayList;
+/**
+ * void add (Task task) - это метод, который добавляет указанную задачу в список.
+ * boolean remove (Task task) - это метод, который удаляет задачу из списка и возвращает true,
+ * если такая задача была в списке. Если в списке несколько задач одного типа, любую из них следует удалить.
+ * int size()- это метод, который возвращает количество задач списка.
+ * Task getTask(int index) - метод, возвращающий задачу, занимающую указанное место в списке;
+ * индекс первой задачи 0.
+ * <p>
+ * Кроме того, приложение должно знать,
+ * какие задачи из списка запланированы хотя бы один раз в определенном интервале,
+ * например, какие задачи запланированы на следующую неделю. Чтобы реализовать это,
+ * создайте the ArrayTaskList incoming(int from, int to) method in the ArrayTaskList class.
+ * Этот метод возвращает подмножество задач, которые запланированы для выполнения
+ * хотя бы один раз после the "from" время, и не позднее "to" time.
+ */
 
 public class ArrayTaskList {
+    private final int arrayTaskListSize = 9;
+    private Task[] tasks = new Task[arrayTaskListSize];
 
-    /*
-    void add (Task task) - это метод, который добавляет указанную задачу в список.
-    boolean remove (Task task) - это метод, который удаляет задачу из списка и возвращает true,
-    если такая задача была в списке. Если в списке несколько задач одного типа, любую из них следует удалить.
-    int size()- это метод, который возвращает несколько задач из списка.
-    Task getTask(int index) - метод, возвращающий задачу, занимающую указанное место в списке; индекс первой задачи 0.
-
-     Кроме того, приложение должно знать,
-     какие задачи из списка запланированы хотя бы один раз в определенном интервале,
-     например, какие задачи запланированы на следующую неделю. Чтобы реализовать это,
-     создайте the ArrayTaskList incoming(int from, int to) method in the ArrayTaskList class.
-     Этот метод возвращает подмножество задач, которые запланированы для выполнения
-     хотя бы один раз после the "from" время, и не позднее "to" time.
-     */
-
-    private ArrayList<Task> Tasks = new ArrayList<Task>();
-
-    void add (final Task task) {
-        if( task.getRepeatInterval() > 0 ) {
-            this.Tasks.add(new Task(
-                    task.getTitle(),
-                    task.getStartTime(),
-                    task.getEndTime(),
-                    task.getRepeatInterval()));
-        } else {
-            this.Tasks.add(new Task(
-                    task.getTitle(),
-                    task.getTime()));
+    void add(final Task task) {
+        for (int i = 0; i < this.arrayTaskListSize; ++i) {
+            if (this.tasks[i] == null) {
+                this.tasks[i] = task;
+                break;
+            }
         }
     }
 
-    public boolean remove (final Task task) {
-        if (task != null) {
-            this.Tasks.remove(task);
-            return true;
+    public boolean remove(final Task task) {
+        boolean contains = false;
+        if (task == null) {
+            return contains;
         } else {
-            return false;
+            for (int i = 0; i < this.tasks.length; ++i) {
+                if (this.tasks[i] == task) {
+                    contains = true;
+                    this.tasks[i] = null;
+                }
+            }
+            return contains;
         }
     }
 
     public int size() {
-        return Tasks.size();
+        return this.tasks.length;
     }
 
     public Task getTask(final int index) {
-        return Tasks.get(index);
+        if (tasks[index] == null) {
+            return new Task("null",0);
+        } else {
+            return this.tasks[index];
+        }
     }
 
-    ArrayList<Task> incoming(final int from, final int to) {
-        ArrayList<Task> incomTasks = new ArrayList<Task>();
-        for (int index = 0; index < Tasks.size(); index += 1) {
-            Task task = getTask(index);
-            int time = task.getTime();
-            if (from <= time & time <= to) {
-                incomTasks.add(task);
+    ArrayTaskList incoming(final int from, final int to) {
+        ArrayTaskList incomTasks = new ArrayTaskList();
+
+        for (int index = 0; index < this.tasks.length; index++) {
+            if (tasks[index] == null) {
+                break;
+            } else {
+                int time = tasks[index].getTime();
+                if (from >= time & time <= to) {
+                    incomTasks.add(tasks[index]);
+                }
             }
         }
         return incomTasks;
