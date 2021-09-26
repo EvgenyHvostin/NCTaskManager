33dc3878be662f14c4167e087c747a74ef08dce7
EvgenyHvostin <evgenyzhvostin@gmail.com>
2021-09-27 02:20:27 +0300

    Uptede

--- src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/TaskIO.java
+++ src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/TaskIO.java
@@ -1,30 +1,79 @@
 package ua.edu.sundu.j2se.studentHvostin.tasks;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
 
 public class TaskIO {
     public static void write(AbstractTaskList tasks, OutputStream out) {
-        try{
-            out.write(tasks.toString().getBytes());
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(out);
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+
+            objectOutputStream.write(gson.toJson(tasks).getBytes());
+            //out.write(gson.toJson(tasks).getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }//– записывает задачи из списка в поток в двоичном формате.
+
+    }// записывает задачи из списка в поток в двоичном формате.
 
     public static void read(AbstractTaskList tasks, InputStream in) {
 
-    }//– считывает задачи из потока в текущий список задач.
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(in);
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+
+            tasks = gson.fromJson(String.valueOf(objectInputStream), AbstractTaskList.class);
+
+
+            //tasks = (AbstractTaskList) objectInputStream.readObject();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }// считывает задачи из потока в текущий список задач.
 
     public static void writeBinary(AbstractTaskList tasks, File file) {
 
-    }//– записывает задачи из списка в файл.
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+
+
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+
+            objectOutputStream.write(gson.toJson(tasks).getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }// записывает задачи из списка в файл.
 
     public static void readBinary(AbstractTaskList tasks, File file) {
 
-    }//– считывает задачи из файла в список задач.
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+
+
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+
+            tasks = gson.fromJson(String.valueOf(objectInputStream), AbstractTaskList.class);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }// считывает задачи из файла в список задач.
 
 }
