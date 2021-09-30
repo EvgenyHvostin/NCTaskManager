c57fd9aae38b4064f46fad8b1d8952faabf59f3d
EvgenyHvostin <evgenyzhvostin@gmail.com>
2021-10-01 00:22:53 +0300

    Update

--- src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/Tasks.java
+++ src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/Tasks.java
@@ -9,13 +9,25 @@ public class Tasks {
     public static Iterable<Task> incoming(
             final Iterable<Task> tasks, final LocalDateTime start, final LocalDateTime end) {
 
-        Iterator<Task> incomTaks = tasks.iterator();
+        Iterator<Task> incomingTask = tasks.iterator();
         Task task;
 
-        while (incomTaks.hasNext()) {
-            task = incomTaks.next();
+        while (incomingTask.hasNext()) {
+            task = incomingTask.next();
             if (!start.isBefore(task.getEndTime()) && !task.getStartTime().isBefore(end)) {
-                incomTaks.remove();
+                if (task.isRepeated()) {
+                    LocalDateTime time = task.nextTimeAfter(start);
+
+                    while (time.isBefore(end)) {
+                        if (time.isAfter(start)) {
+                            incomingTask.remove();
+                            break;
+                        }
+                        time = task.nextTimeAfter(time);
+                    }
+                } else {
+                    incomingTask.remove();
+                }
             }
         }
         return tasks;
@@ -30,30 +42,41 @@ public class Tasks {
         Task task;
 
         while (iter.hasNext()) {
-
             task = iter.next();
-            if (task.isRepeated()) {
-                if (start.isBefore(task.getEndTime()) && task.getStartTime().isBefore(end)) {
 
-                    LocalDateTime time = task.nextTimeAfter(start);
-                    final Set<String> set = new TreeSet<>();
+            if (start.isBefore(task.getEndTime()) && task.getStartTime().isBefore(end)) {
+                if (task.isRepeated()) {
+                    if (task.nextTimeAfter(start).isBefore(end)) {
+                        LocalDateTime time = task.nextTimeAfter(start);
 
-                    set.add(task.getTitle());
-                    while (time.isBefore(task.getEndTime())) {
-                        calendar.put(time, set);
-                        time = time.plusDays(task.getRepeatInterval());
+                        while (time.isBefore(task.getEndTime())) {
+                            if (calendar.containsKey(task.getTime())) {
+                                final Set<String> set = calendar.get(task.getTime());
+
+                                set.add(task.getTitle());
+                            } else {
+                                final Set<String> set = new TreeSet<>();
+
+                                set.add(task.getTitle());
+                                calendar.put(task.getTime(), set);
+                            }
+                            time = task.nextTimeAfter(time);
+                        }
                     }
-                }
-            } else {
-
-                if (start.isBefore(task.getTime()) && task.getTime().isBefore(end)) {
-                    final Set<String> set = new TreeSet<>();
-                    set.add(task.getTitle());
-                    calendar.put(task.getTime(), set);
+                } else {
+                    if (calendar.containsKey(task.getTime())) {
+                        final Set<String> set = calendar.get(task.getTime());
+                        set.add(task.getTitle());
+                    } else {
+                        final Set<String> set = new TreeSet<>();
+                        set.add(task.getTitle());
+                        calendar.put(task.getTime(), set);
+                    }
                 }
             }
         }
-
         return calendar;
+
     }
+
 }
---
+++ src/test/java/ua/edu/sundu/j2se/studentHvostin/tasks/TestTaskIO.java
@@ -0,0 +1,35 @@
+package ua.edu.sundu.j2se.studentHvostin.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.SortedMap;
+
+public class TestTaskIO {
+
+    @Test
+    public void testIO() throws IOException {
+/*
+        ArrayTaskList array = new ArrayTaskList();
+        LocalDateTime time = LocalDateTime.of(1,1,1,1,1);
+        InputStream in;
+        File file;
+        try (OutputStream out = new ObjectOutputStream()) {
+
+            array.add(new Task("task0", time));
+            array.add(new Task("task1", time));
+
+            TaskIO.write(array, out);
+        }
+        TaskIO.read(array, in);
+        TaskIO.writeBinary(array, file);
+        TaskIO.readBinary(array, file);
+
+
+        //Assertions.assertEquals(iter.next(), testTask);
+
+ */
+    }
+}
