6c7d2906669666145cb3a9bf82a99cbfa69adb27
EvgenyHvostin <evgenyzhvostin@gmail.com>
2021-10-01 03:37:02 +0300

    Update

--- src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/TaskIO.java
+++ src/main/java/ua/edu/sundu/j2se/studentHvostin/tasks/TaskIO.java
@@ -2,73 +2,70 @@ package ua.edu.sundu.j2se.studentHvostin.tasks;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.io.*;
 
 public class TaskIO {
-    public static void write(final AbstractTaskList tasks, OutputStream out) {
-        try {
-
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(out);
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
-
-            objectOutputStream.write(gson.toJson(tasks).getBytes());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public static void write(AbstractTaskList tasks, OutputStream out) throws IOException {
+        ObjectOutputStream objectStream = new ObjectOutputStream(out);
+        objectStream.writeObject(tasks);
+        objectStream.flush();
     }
 
-    public static void read(AbstractTaskList tasks, final InputStream in) {
+    public static void read(AbstractTaskList tasks, InputStream in) throws IOException, ClassNotFoundException {
+        ObjectInputStream objectStream = new ObjectInputStream(in);
+        ArrayTaskList readTasks = (ArrayTaskList) objectStream.readObject();
+        objectStream.close();
 
-        try {
-            ObjectInputStream objectInputStream = new ObjectInputStream(in);
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
-            AbstractTaskList readTasks;
+        for (int i = 0; i <= readTasks.getSize(); i++) {
+            tasks.add(readTasks.getTask(i));
+        }
+    }
 
-            readTasks = gson.fromJson(String.valueOf(objectInputStream), AbstractTaskList.class);
-
-            for (int i = 0; i <= readTasks.getSize(); i++) {
-                tasks.add(readTasks.getTask(i));
-            }
-        } catch (IOException e) {
+    public static void writeBinary( AbstractTaskList tasks, File file) {
+        try (BufferedWriter w = new BufferedWriter(new FileWriter(String.valueOf(file)))) {
+            write(tasks, w);
+        } catch (final IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void writeBinary(final AbstractTaskList tasks, File file) {
-
-        try {
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
-
-            objectOutputStream.write(gson.toJson(tasks).getBytes());
-
-        } catch (IOException e) {
+    public static void readBinary(AbstractTaskList tasks, File file) {
+        try (BufferedReader r = new BufferedReader(new FileReader(String.valueOf(file)))) {
+            read(tasks, r);
+        } catch (final IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    public static void readBinary(AbstractTaskList tasks, final File file) {
 
-        try {
-            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
-            AbstractTaskList readTasks;
+    public static void write(AbstractTaskList tasks, Writer out) throws IOException {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        out.append(gson.toJson(tasks));
+    }
 
-            readTasks = gson.fromJson(String.valueOf(objectInputStream), AbstractTaskList.class);
+    public static void read(AbstractTaskList tasks, Reader in) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        AbstractTaskList readTasks = gson.fromJson(in, AbstractTaskList.class);
 
-            for (int i = 0; i <= readTasks.getSize(); i++) {
-                tasks.add(readTasks.getTask(i));
-            }
+        for (int i = 0; i <= readTasks.getSize(); i++) {
+            tasks.add(readTasks.getTask(i));
+        }
+    }
 
-        } catch (IOException e) {
+    public static void writeText(AbstractTaskList tasks, File file) {
+        try (BufferedWriter w = new BufferedWriter(new FileWriter(String.valueOf(file)))) {
+            write(tasks, w);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readText(AbstractTaskList tasks, File file) {
+        try (BufferedReader r = new BufferedReader(new FileReader(String.valueOf(file)))) {
+            read(tasks, r);
+        } catch (final IOException e) {
             e.printStackTrace();
         }
     }
--- src/test/java/ua/edu/sundu/j2se/studentHvostin/tasks/TestTaskIO.java
+++ src/test/java/ua/edu/sundu/j2se/studentHvostin/tasks/TestTaskIO.java
@@ -1,5 +1,6 @@
 package ua.edu.sundu.j2se.studentHvostin.tasks;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -11,24 +12,25 @@ public class TestTaskIO {
 
     @Test
     public void testIO() throws IOException {
+
+        AbstractTaskList tasks = new ArrayTaskList();
+        AbstractTaskList newTasks = new ArrayTaskList();
+        File file = new File("testTaskIO.out");
+
+        tasks.add(new Task("task0", LocalDateTime.of(1,1,1,1,1)));
+        tasks.add(new Task("task1", LocalDateTime.of(2,2,2,2,2)));
+
+        TaskIO.writeText(tasks, file);
+        TaskIO.readText(newTasks, file);
+
+        //Assertions.assertEquals(tasks, newTasks);
+
 /*
-        ArrayTaskList array = new ArrayTaskList();
-        LocalDateTime time = LocalDateTime.of(1,1,1,1,1);
-        InputStream in;
-        File file;
-        try (OutputStream out = new ObjectOutputStream()) {
 
-            array.add(new Task("task0", time));
-            array.add(new Task("task1", time));
-
-            TaskIO.write(array, out);
-        }
-        TaskIO.read(array, in);
-        TaskIO.writeBinary(array, file);
-        TaskIO.readBinary(array, file);
-
-
-        //Assertions.assertEquals(iter.next(), testTask);
+        write(AbstractTaskList tasks, Writer out);
+        read(AbstractTaskList tasks, Reader in);
+        writeText(AbstractTaskList tasks, File file);
+        readText(AbstractTaskList tasks, File file);
 
  */
     }
